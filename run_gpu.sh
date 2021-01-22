d1c5f96c57880baf81aab3fa1609a19a251c2710
brian.nghiem <>
2021-01-22 02:09:17 -0500

    working batch script, NB. explicit path to conda env

--- run_gpu.sh
+++ run_gpu.sh
@@ -5,7 +5,7 @@
 #SBATCH -p gpu #partition
 #SBATCH --gres=gpu:1               # max 3 GPUs
 #SBATCH --cpus-per-task=20         # max 41 CPUs
-#SBATCH --mem=32G              # max > 200GB memory per node
+#SBATCH --mem=100G              # max > 200GB memory per node
 #SBATCH --time=24:00:00 #wall time
 #SBATCH --mail-user=brian.nghiem@rmp.uhn.ca
 #SBATCH --mail-type=ALL #mail notifications
@@ -14,7 +14,11 @@
 export HOME=/cluster/projects/uludag/Brian/moco-sigpy/namer
 cd ${HOME}
 
-conda activate sigpy_env
+conda info --envs
+'''Output:
+base                  *  /cluster/tools/software/centos7/python/3.7.2
+'''
+conda activate /cluster/home/brian.nghiem/.conda/envs/sigpy_env
 echo "now processing task id:: " ${SLURM_JOBID}
 echo "--------------------------"
 
